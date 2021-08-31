56e81d76447bdf4dce3ac9d6b18ea4905ea223c5
Jesul1 <omajesse.vaarala@gmail.com>
2021-08-31 12:04:05 +0300

    Photon hommat

---
+++ Assets/NetworkManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class NetworkManager : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        OnConnectedToServer(); 
+    }
+
+    private void OnConnectedToServer()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+        Debug.Log("Try connect to server...");
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Connected to server.");
+        base.OnConnectedToMaster();
+        RoomOptions roomOptions = new RoomOptions();
+        roomOptions.MaxPlayers = 10;
+        roomOptions.IsVisible = true;
+        roomOptions.IsOpen = true;
+        PhotonNetwork.JoinOrCreateRoom("Room 1", roomOptions, TypedLobby.Default);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("Joined a Room");
+        base.OnJoinedRoom();
+    }
+
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        Debug.Log("A player Joined the room");
+        base.OnPlayerEnteredRoom(newPlayer);
+    }
+}
