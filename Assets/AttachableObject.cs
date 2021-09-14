3f74d66e62fd8ba4ad4845b8b0ebe18344153482
Tonttu81 <joni.pylkkonen@gmail.com>
2021-09-14 15:18:16 +0300

    Object generaattori script on nyt hyvä
    aloitettu vähän objektien crafting systeemiä

---
+++ Assets/AttachableObject.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class AttachableObject : MonoBehaviour
+{
+    [SerializeField] bool inHand;
+
+    public GameObject[] attachmentPoints;
+
+    XRGrabInteractable grabScript;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < attachmentPoints.Length; i++)
+        {
+
+        }
+    }
+
+    public void objGrabbed()
+    {
+        inHand = true;
+    }
+
+    public void objDropped()
+    {
+        inHand = false;
+    }
+}
---
+++ Assets/AttachmentPoint.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttachmentPoint : MonoBehaviour
+{
+    public bool objInRadius;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "AttachmentPoint")
+        {
+            objInRadius = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        objInRadius = false;
+    }
+}
