119ef598b27e9451af44db79d7a538c1116faef2
Jesul1 <omajesse.vaarala@gmail.com>
2021-09-14 15:12:03 +0300

    -Kuuset
    -continious movement
    -snap turn
    -saari näyttää taas paremmalta jostain syystä

--- Assets/Scripts/World Gen Scripts/ObjectGenerator.cs
+++ Assets/Scripts/World Gen Scripts/ObjectGenerator.cs
@@ -4,49 +4,72 @@ using UnityEngine;
 
 public class ObjectGenerator : MonoBehaviour
 {
-    public GameObject[] objects;
+    public Object[] objects;
 
     public int objectDensity;
 
-    public float treeMinSpawnHeight;
-    public float treeMaxSpawnHeight;
-
     private void Start()
     {
-        GenerateTrees();
+        GenerateObjects();
     }
 
-    public void GenerateTrees()
+    public void GenerateObjects()
     {
         int seed = GameObject.FindObjectOfType<MapGenerator>().seed;
 
+        System.Random orng = new System.Random(seed);
 
-        System.Random trng = new System.Random(seed);
+        for (int i = 0; i < objects.Length; i++)
+        {
+            for (int j = 0; j < objects[i].objectDensity; j++)
+            {
+                float x = orng.Next(-900, 900);
+                float y = orng.Next(-900, 900);
+
+                //int objectId = orng.Next(0, objects.Length);
+
+                RaycastHit objectRay;
+                if (Physics.Raycast(new Vector3(x, 100, y), Vector3.down * 100, out objectRay, Mathf.Infinity))
+                {
+                    if (objectRay.point.y > objects[i].objMinSpawnHeight && objectRay.point.y < objects[i].objMaxSpawnHeight)
+                    {
+
+                        float yRotation = orng.Next(0, 360);
+
+                        GameObject tree = Instantiate(objects[i].obj, objectRay.point, Quaternion.Euler(new Vector3(0, yRotation, 0)));
+
+
+                        int rnd = orng.Next(-5, 5);
+
+                        float size = (float)rnd / 10;
+
+                        tree.transform.localScale += new Vector3(size, size, size);
+
+                    }
+                }
+            }
+        }
 
         for (int i = 0; i < objectDensity; i++)
         {
-            float x = trng.Next(-900, 900);
-            float y = trng.Next(-900, 900);
 
-            RaycastHit treeRay;
-            if (Physics.Raycast(new Vector3(x, 100, y), Vector3.down * 100, out treeRay, Mathf.Infinity))
-            {
-                if (treeRay.point.y > treeMinSpawnHeight && treeRay.point.y < treeMaxSpawnHeight)
-                {
-                    int treeId = trng.Next(0, objects.Length);
-                    float yRotation = trng.Next(0, 360);
+        }
+    }
 
-                    GameObject tree = Instantiate(objects[treeId], treeRay.point, Quaternion.Euler(new Vector3(0, yRotation, 0)));
+    [System.Serializable]
+    public struct Object
+    {
+        public GameObject obj;
+        public float objMinSpawnHeight;
+        public float objMaxSpawnHeight;
+        public float objectDensity;
 
-                    
-                    int rnd = trng.Next(-5, 5);
-
-                    float size = (float)rnd / 10;
-
-                    tree.transform.localScale += new Vector3(size, size, size);
-                    
-                }
-            }
+        public Object(GameObject _obj, float _objMinSpawnHeight, float _objMaxSpawnHeight, float _objectDensity)
+        {
+            obj = _obj;
+            objMinSpawnHeight = _objMinSpawnHeight;
+            objMaxSpawnHeight = _objMaxSpawnHeight;
+            objectDensity = _objectDensity;
         }
     }
 }
